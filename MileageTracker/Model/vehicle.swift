259edd4382b9977e84d5f1dd001820ca32be1eb7
Bop <collin.cannavo@gmail.com>
2017-11-21 20:48:34 -0700

    Added model

---
+++ MileageTracker/Model/vehicle.swift
@@ -0,0 +1,27 @@
+//
+//  vehicle.swift
+//  MileageTracker
+//
+//  Created by Collin Cannavo on 11/21/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class Vehicle {
+    
+    let vehicleName: String
+    var mileage: Int
+    
+    init(vehicleName: String, mileage: Int) {
+        self.mileage = mileage
+        self.vehicleName = vehicleName
+        
+        
+    }
+    
+    
+    
+}
