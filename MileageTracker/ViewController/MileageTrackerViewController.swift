478d9e3cbfe9da987ac68142d439626a038f2f7a
Bop <collin.cannavo@gmail.com>
2017-11-21 21:06:30 -0700

    Added view controller

---
+++ MileageTracker/ViewController/MileageTrackerViewController.swift
@@ -0,0 +1,42 @@
+//
+//  MileageTrackerViewController.swift
+//  MileageTracker
+//
+//  Created by Collin Cannavo on 11/21/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class MileageTrackerViewController: UIViewController {
+
+    @IBOutlet weak var vehicleTextField: UITextField!
+    @IBOutlet weak var mileageTextField: UITextField!
+    @IBAction func submitButtonTapped(_ sender: Any) {
+        
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
