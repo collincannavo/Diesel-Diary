b8541ae4cabda2158d4cb0d3efc2d666c884ebef
Bop <collin.cannavo@gmail.com>
2017-11-29 20:12:02 -0700

    Adding NSURL session for networking

--- MileageTracker/ViewController/MileageTrackerViewController.swift
+++ MileageTracker/ViewController/MileageTrackerViewController.swift
@@ -11,10 +11,49 @@ import Foundation
 
 class MileageTrackerViewController: UIViewController {
 
+    let url = "http://localhost..."
     
     @IBOutlet weak var vehicleTextField: UITextField!
     @IBOutlet weak var mileageTextField: UITextField!
+   
     @IBAction func submitButtonTapped(_ sender: Any) {
+        
+        let requestURL = NSURL(fileURLWithPath: url)
+        
+        let request = NSMutableURLRequest(url: requestURL as URL!)
+        
+        request.httpMethod = "POST"
+        
+        let teamName = vehicleTextField.text
+        let memberCount = mileageTextField.text
+        
+        let postParamaters = "name="+teamName!+"&member="+memberCount!
+        
+        request.httpBody = postParamaters.data(using: String.Encoding.utf8)
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+        
+            
+            if error != nil {
+                print("error is \(error)")
+                return;
+            }
+            
+            do {
+                let myJSON = try JSONSerialization.jsonObject(with: data, options: .mutableContainers) as? NSDictionary
+                
+                if let parseJSON = myJSON {
+                    var msg: String!
+                    msg = parseJSON["message"] as! String
+                    
+                    print(msg)
+                }
+            } catch {
+                print(error)
+            }
+        }
+        dataTask.resume()
         
     }
     
